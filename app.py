4f0702ad540d63ffa3fdb4b8c31b0cfc0694aa2d
12357David19 <davejnewman19@gmail.com>
2017-06-19 19:42:18 -0500

    played around with different endpoints

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
-@app.route('/')
+file = '/static/example_scout.jpg'
+labels = ["Head","Lungs","Abdomen","Pelvis"]
+# @app.route('/')
+@app.route('/', methods=['POST'])
 def index():
-	file = 'smiley.gif'
-	return render_template('index.html', filename=file)
+	if method = "post":
+		for index,label in enumerate(labels):
+			print("%s: %s"% (label,request.form[label]))
+	return render_template('index.html', filename=file,labels=labels)
+
+def submit():
+
+	return render_template('index.html', filename=file,labels=labels)
 
 if __name__ == '__main__':
     app.run()
