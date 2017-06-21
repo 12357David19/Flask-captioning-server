1ac911d8f9dd2ab2b2a93febecaeba435fd93c97
Justin Miller <justin.miller@marquette.edu>
2017-06-20 20:15:20 -0500

    cleaned up some stuff

--- app.py
+++ app.py
@@ -3,16 +3,20 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 file = '/static/example_scout.jpg'
 labels = ["Head","Lungs","Abdomen","Pelvis"]
-# @app.route('/')
-@app.route('/', methods=['POST'])
+
+def get_file():
+	return file
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-	if method = "post":
-		for index,label in enumerate(labels):
-			print("%s: %s"% (label,request.form[label]))
-	return render_template('index.html', filename=file,labels=labels)
+	filename = get_file()
+	return render_template('index.html', filename=filename,labels=labels)
 
+@app.route('/submit', methods=['POST'])
 def submit():
-
+	if request.method == "post":
+		for index,label in enumerate(labels):
+			print("%s: %s"% (label,request.form['form']))
 	return render_template('index.html', filename=file,labels=labels)
 
 if __name__ == '__main__':
