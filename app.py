c7acbdff05accf6056d51f762f3b1a2b4ad0d197
Newman <212581462@logon.ds.ge.com>
2017-06-23 18:29:19 -0500

    app.py index.html added 404 page

--- app.py
+++ app.py
@@ -1,38 +1,54 @@
 from flask import Flask, render_template, request
-
+import json
+import csv
 app = Flask(__name__)
-file = '/static/example_scout.jpg'
-labels = ["Head", "Lungs", "Abdomen", "Pelvis"]
+input_list = 'to_be_labeled.csv'
+labels = ["head", "lung", "abdomen", "pelvis"]
 
 # unlabeled_list = "unlabeled"
 # labeling_list = "labeling"
 # labeled_list = "labeled"
-unlabeled_list = [file]
+unlabeled_list = []
+with open(input_list, 'r') as f:
+    for line in f:
+        unlabeled_list.append(line.replace('\n', ''))
 labeling_list = []
-labeled_list = []
+fn = ['filename', 'is_head', 'is_lung', 'is_abdomen', 'is_pelvis']
+with open('labeled.csv', 'wb') as f:
+    writer = csv.DictWriter(f, fieldnames=fn)
+    writer.writeheader()
 
 
 def get_file():
     """get file should return a file from the unlabeled dir, move file to """
-    file = unlabeled_list.pop()
+    try:
+        file = unlabeled_list.pop()
+    except IndexError as e:
+        return "empty list"
     labeling_list.append(file)
+    with open('labeling.txt', 'a') as f:
+        f.write(file + '\n')
+    # if file ==
     return file
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET'])
 def index():
     filename = get_file()
+    if filename == "empty list":
+        return render_template('empty.html')
     return render_template('index.html', filename=filename, labels=labels)
 
 
 @app.route('/submit', methods=['POST'])
 def submit():
     if request.method == "POST":
-        for index, label in enumerate(labels):
-            if label in request.form:
-                print(label)
-            # print("%s: %s" % (label, request.form['form']))
-    return render_template('index.html', filename=file, labels=labels)
+        data = json.loads(request.data)
+        with open('labeled.csv', 'a') as f:
+            writer = csv.DictWriter(f, fieldnames=fn)
+            writer.writerow(data)
+
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
