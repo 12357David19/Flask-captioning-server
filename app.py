8c23cdfc70cc2c1dacaec89ac2af0ef2c3723e01
LOGON\212581462 <david.newman@ge.com>
2017-06-27 09:23:33 -0500

    playing with dicom conversion

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template, request
 import json
+import os
+import dicom
 import csv
+import numpy as np
+from cv2 import imwrite
 app = Flask(__name__)
-input_list = 'to_be_labeled.csv'
-labels = ["head", "lung", "abdomen", "pelvis"]
+hosted_dir = os.path.abspath("static") + '/'
+labels = ["head", "lung", "abdomen", "pelvis"] # user-facing labels
+data_location = "C:\Users\\212581462\Documents\Scout-data/"
 
-# unlabeled_list = "unlabeled"
-# labeling_list = "labeling"
-# labeled_list = "labeled"
-unlabeled_list = []
-with open(input_list, 'r') as f:
-    for line in f:
-        unlabeled_list.append(line.replace('\n', ''))
+def get_dir_list(path):
+    l = os.listdir(path)
+    output = []
+    for dcm in l:
+        output.append(data_location + dcm)
+    return output
+
+unlabeled_list = get_dir_list(data_location)
 labeling_list = []
-fn = ['filename', 'is_head', 'is_lung', 'is_abdomen', 'is_pelvis']
+fn = ['filename', 'is_head', 'is_lung', 'is_abdomen', 'is_pelvis'] #server/csv side labels
 with open('labeled.csv', 'wb') as f:
     writer = csv.DictWriter(f, fieldnames=fn)
     writer.writeheader()
@@ -25,11 +31,29 @@ def get_file():
         file = unlabeled_list.pop()
     except IndexError as e:
         return "empty list"
+    #taking dicom file and creating a png and displaying it
+    if ".dcm" in os.path.basename(file):
+        d = dicom.read_file(file)
+        d_array = d.pixel_array
+        filename =os.path.basename(file.replace(".dcm", ".png"))
+        png_name = os.path.join(hosted_dir + filename)
+        imwrite(png_name, d_array)
+        file = png_name
+
     labeling_list.append(file)
-    with open('labeling.txt', 'a') as f:
-        f.write(file + '\n')
-    # if file ==
     return file
+
+
+def get_file_data(json):
+    png_file = json['filename']
+    file_name = os.path.basename(json['filename'])
+    dcm_path = data_location + file_name.replace(".png",".dcm")
+    json['filename'] = dcm_path
+    with open('labeled.csv', 'a') as f:
+        writer = csv.DictWriter(f, fieldnames=fn)
+        writer.writerow(json)
+    labeling_list.remove(png_file)
+    os.remove(png_file)
 
 
 @app.route('/', methods=['GET'])
@@ -44,9 +68,7 @@ def index():
 def submit():
     if request.method == "POST":
         data = json.loads(request.data)
-        with open('labeled.csv', 'a') as f:
-            writer = csv.DictWriter(f, fieldnames=fn)
-            writer.writerow(data)
+        get_file_data(data)
 
     return render_template('index.html')
 
